17eb6b2242d57b28f8c31522b9e0a2fb06422bdc
mattayes <matt.hayes91@gmailcom>
2014-12-22 14:02:30 -0800

    Monkeying around with plots

--- methods.R
+++ methods.R
@@ -71,4 +71,52 @@ for(i in 1:51){
     assign(paste("df", as.character(i), sep = ""), json$agencies[[i]])
 }
 
-## 
+#######################
+## Plot
+
+## Hack to ordering the bars by kohske : http://stackoverflow.com/a/5414445/1457051 #####
+
+oregon <- mutate(oregon, cat2 = reorder(factor(paste(aid, category)), rank(-value)))
+
+## Pretty names
+
+levels(oregon$category) <- c("Weapons", "Travel, training", "Other",
+                           "Communications, computers", "Building improvements",
+                           "Electronic surveillance", "Information, rewards",
+                           "Salary, overtime", "Community programs")
+
+## Replace NAs with 0
+na <- is.na(oregon$value)
+oregon$value[na] <- 0
+
+## Limit plots to greater than 0
+gt0 <- oregon$value > 0
+limit <- unique(oregon$cat2[gt0])
+
+# plot totals w/o considerig per-capita #####
+gt0 <- oregon
+
+## Plot fail
+
+gg <- ggplot(oregon, aes(cat2, value)) +
+    geom_bar(stat = "identity", aes(fill = category)) +
+    scale_y_continuous(labels = dollar) + 
+    scale_x_discrete(breaks = oregon$cat2, labels = oregon$aid) + 
+    facet_grid(category ~ ., scales = "free")
+gg <- gg + theme_bw()
+gg <- gg + theme(strip.background=element_blank())
+gg <- gg + theme(strip.text=element_text(size=15, face="bold"))
+gg <- gg + theme(panel.margin=unit(2, "lines"))
+gg <- gg + theme(panel.border=element_blank())
+gg <- gg + theme(axis.text.x=element_text(size=9))
+gg <- gg + theme(legend.position="none")
+gg
+
+## Better, but I want non-continuous bins
+ggplot(oregon, aes(cat2, value)) +
+    geom_bar(stat = "identity", aes(fill = category)) +
+    stat_bin(drop = TRUE) +
+    scale_y_continuous(labels = dollar) +
+    scale_x_discrete(labels = oregon$aid, breaks = oregon$cat2) +
+    facet_wrap(~ category, scales = "free", ncol = 1)
+
